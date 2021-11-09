5e304abd1bd2856c1a9f9f4d994b6354ad17cf14
Wagner Goulart <wcfx.dev@gmail.com>
2021-11-09 17:44:05 -0300

    build: default structure

---
+++ src/modules/Home/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// import {  } from '~/components';
+// import {  } from '~/assets';
+// import {  } from '~/helpers';
+// import {  } from '~/types';
+import * as S from './styles';
+
+const Home = () => {
+  return (
+    <S.Container>
+      <S.Title>Hello Motherfuckers</S.Title>
+      <S.Description>Wagnifico na parada</S.Description>
+    </S.Container>
+  );
+};
+export { Home };
