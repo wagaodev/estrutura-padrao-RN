9c0cf314df213a5630476f547cf9cd0ed7ef0907
Wagner Goulart <wcfx.dev@gmail.com>
2021-11-26 01:31:05 -0300

    chore: everything ok

--- src/modules/Home/index.tsx
+++ src/modules/Home/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import Icon from 'react-native-vector-icons/Feather';
 
 // import {  } from '~/components';
@@ -12,7 +13,7 @@ const Home = () => {
     <S.Container>
       <S.Title>Hello Motherfuckers</S.Title>
       <S.Description>Wagnifico na parada</S.Description>
-      <Icon name="home" size={20} color="#fff" />
+      <Icon name="home" size={24} color="#fff" />
     </S.Container>
   );
 };
