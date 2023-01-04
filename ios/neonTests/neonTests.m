9b736e4e319fbeec26128ac97e26d33246a0245b
wagaodev <wcfx.dev@gmail.com>
2023-01-03 23:40:20 -0300

    chore: change the project name

--- ios/neonTests/neonTests.m
+++ ios/neonTests/neonTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface uberTests : XCTestCase
+@interface neonTests : XCTestCase
 
 @end
 
-@implementation uberTests
+@implementation neonTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
